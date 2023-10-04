7dd82fbb388dbf1dcd45ef31983b1c8da3364b36
guillermooom <97970403+guillermooom@users.noreply.github.com>
2023-10-04 12:09:26 +0200

    Add files via upload

--- Ejercicios fork/fork3.c
+++ Ejercicios fork/fork3.c
@@ -3,36 +3,45 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
-void main(){
-	pid_t pid1,pid2,pid3;
+void main(){					//P1
+	pid_t pid1,pid2,pid3,pid4,pid5;
 	
-	pid1 = fork();
+	pid1 = fork();				//P2
 		
 	if(pid1==0){
-		printf("Mi PID es %d\n",getpid());
-		printf("El PID de mi padre es %d\n\n",getppid());
-		for(int i=0;i<2;i++){
-			pid2 = fork();
-		}
-		/*if(pid2==0){
-			printf("Mi PID es %d\n",getpid());
-			printf("El PID de mi padre es %d\n\n",getppid());
-			pid3 = fork();
-			if(pid3==0){
-				printf("Mi PID es %d\n",getpid());
-				printf("El PID de mi padre es %d\n\n",getppid());
-			}else{
-				wait(0);
-				printf("Soy el padre y tengo el %d\n",getpid());
-			}
-		}else{
+		printf("Mi PID es el %d\n",getpid());
+		printf("Mi padre tiene el %d\n",getppid());
+		
+		printf("Acaba P2\n\n");	//ACABA P2
+		
+		pid2 = fork();			//P3
+		
+		if(pid2==0){
+			printf("aa-------");
+			printf("Mi PID es el %d\n",getpid());
+			printf("Mi padre tiene el %d\n",getppid());
+			printf("Acaba P3\n\n");//ACABA P3
+		}else if(pid2!=0){
 			wait(0);
-			printf("Soy el padre y tengo el %d\n",getpid());
-		}*/
 			
+		}
+		
+		pid3 = fork();			//P4
+		
+		if(pid3==0 && pid2!=0){
+		printf("bb-------");
+			printf("Mi PID es el %d\n",getpid());
+			printf("Mi padre tiene el %d\n",getppid());
+			printf("Acaba P4\n\n");//ACABA P4
+		}else if(pid3!=0){
+			wait(0);
+			
+		}
 	}else{
 		wait(0);
 		printf("Soy el padre y tengo el %d\n",getpid());
+		
+		printf("Acaba P1\n\n");	//ACABA P1
 	}
 
  	exit(0);
