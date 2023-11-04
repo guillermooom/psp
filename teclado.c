cee2958030c2af8809e43d07c36460e36409fb2e
guillermooom <97970403+guillermooom@users.noreply.github.com>
2023-11-04 22:42:58 +0100

    Add files via upload

---
+++ teclado.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Ejemplo aprenderaprogramar.com
+int main() {
+    int valorUsuario;
+    printf("Introduzca un numero entero: ");
+    scanf("%d", &valorUsuario);
+    printf("%i",valorUsuario);
+    return 0;
+}
